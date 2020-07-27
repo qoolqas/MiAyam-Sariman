7f4b7f1ecc0bfca011e56dd9cdf63f3238564ab8
Qoolqas <qoolqas@gmail.com>
2020-07-27 07:50:38 +0700

    fix pager decoration rv

--- app/src/main/java/com/qoolqas/miayam/MainActivity.kt
+++ app/src/main/java/com/qoolqas/miayam/MainActivity.kt
@@ -1,6 +1,10 @@
 package com.qoolqas.miayam
 
 import android.os.Bundle
+import android.view.View
+import android.view.Window
+import android.view.WindowManager
+import android.widget.Toolbar
 import com.google.android.material.bottomnavigation.BottomNavigationView
 import androidx.appcompat.app.AppCompatActivity
 import androidx.navigation.findNavController
@@ -14,13 +18,14 @@ class MainActivity : AppCompatActivity() {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
         val navView: BottomNavigationView = findViewById(R.id.nav_view)
-
         val navController = findNavController(R.id.nav_host_fragment)
+        supportActionBar?.hide()
         // Passing each menu ID as a set of Ids because each
         // menu should be considered as top level destinations.
         val appBarConfiguration = AppBarConfiguration(setOf(
                 R.id.navigation_home, R.id.navigation_dashboard, R.id.navigation_notifications))
         setupActionBarWithNavController(navController, appBarConfiguration)
         navView.setupWithNavController(navController)
+
     }
 }
