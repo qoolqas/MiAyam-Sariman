b5823bf804ce3a0332abb8a0997255335d3c3e32
Qoolqas <qoolqas@gmail.com>
2020-08-13 23:36:30 +0700

    minuman

--- app/src/main/java/com/qoolqas/miayam/ui/dashboard/DashboardFragment.kt
+++ app/src/main/java/com/qoolqas/miayam/ui/dashboard/DashboardFragment.kt
@@ -6,8 +6,19 @@ import android.view.View
 import android.view.ViewGroup
 import androidx.fragment.app.Fragment
 import com.qoolqas.miayam.R
+import com.qoolqas.miayam.model.Data
 
 class DashboardFragment : Fragment() {
+    private val mNicolasCageMovies = listOf(
+        Data(R.drawable.mie1, "Mie Ayam","10000","Mie Ayam"),
+        Data(R.drawable.mie2, "Mie Ayam","10000","Mie Ayam"),
+        Data(R.drawable.mie3, "Mie Ayam","10000","Mie Ayam"),
+        Data(R.drawable.mie1, "Mie Ayam","10000","Mie Ayam"),
+        Data(R.drawable.mie2, "Mie Ayam","10000","Mie Ayam"),
+        Data(R.drawable.mie3, "Mie Ayam","10000","Mie Ayam"),
+        Data(R.drawable.mie1, "Mie Ayam","10000","Mie Ayam"),
+        Data(R.drawable.mie2, "Mie Ayam","10000","Mie Ayam"),
+        Data(R.drawable.mie3, "Mie Ayam","10000","Mie Ayam")
 
     private lateinit var dashboardViewModel: DashboardViewModel
 
@@ -18,5 +29,9 @@ class DashboardFragment : Fragment() {
     ): View? {
 
         return inflater.inflate(R.layout.fragment_dashboard, container, false)
+    }
+
+    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
+        super.onViewCreated(view, savedInstanceState)
     }
 }
